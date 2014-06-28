fd8ffdd0bf2d944f7d7d8fe21afb59a9a52e08fb
jvaleo <jvaleo@mac.com>
2014-06-28 14:59:41 -0400

    Working on method for determining which train to take

--- subway_status.py
+++ subway_status.py
@@ -1,10 +1,27 @@
 from lxml import etree
 
-def subway_status():
+def subway_status(line):
     xml_base =  etree.parse("http://web.mta.info/status/serviceStatus.txt")
     root = xml_base.getroot()
+    #move the above to an in memory db, fetch every 5 minutes and update
     last_updated = root[1].text
     for subway_element in root[2]:
         subway_line = subway_element[0].text
-	subway_service_status = subway_element[1].text
-	return last_updated, subway_line, subway_service_status
+	if line == subway_line:
+            subway_service_status = subway_element[1].text
+	    return subway_service_status
+
+def get_best_route(preferred_subway_lines):
+    """
+    Take in a list of lines, and only return those that are in good service
+    """
+    if len(preferred_subway_lines) == 1:
+        #Return that there is only one route
+	return str('Please add multiple routes')
+    for line in preferred_subway_lines:
+	line = line.upper()
+        subway_service_status = subway_status(line)
+	if subway_service_status == "GOOD SERVICE":
+	    print 'Status for {} is {}'.format(line, subway_service_status)
+	else:
+	    print 'Status for {} is {}'.format(line, subway_service_status)
