26f7d87994af29455f1f9ce2afd84bbed325075a
jvaleo <jvaleo@mac.com>
2014-06-28 14:45:35 -0400

    Typo

--- subway_status.py
+++ subway_status.py
@@ -1,7 +1,7 @@
 from lxml import etree
 
 def subway_status():
-    xml_base = tree = etree.parse("http://web.mta.info/status/serviceStatus.txt")
+    xml_base =  etree.parse("http://web.mta.info/status/serviceStatus.txt")
     root = xml_base.getroot()
     last_updated = root[1].text
     for subway_element in root[2]:
